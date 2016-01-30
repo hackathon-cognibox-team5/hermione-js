727344f16b2f95ab0a5bed2af944d02401c9657b
Philippe Lafontaine <lafontaine.philippe@gmail.com>
2016-01-30 14:12:37 -0500

    extends has name

--- jsModel.js
+++ jsModel.js
@@ -42,31 +42,34 @@ var JsModel = {
   },
 
   /*
-    var User = Model.extend({
-      name: "User"
-    }, {
-      fullName: function() { return this.firstName + this.lastName; }
-    }, {
-      get: function(options) { return this.sync("read", null, options); }
+  var User = Model.extend({
+  name: "User"
+}, {
+fullName: function() { return this.firstName + this.lastName; }
+}, {
+get: function(options) { return this.sync("read", null, options); }
+});
+*/
+extend: function(configuration, instanceMethods, classMethods) {
+  var instanceObj = _.extend({}, this.$instance, classMethods);
+  var classObj = _.extend({}, this, classMethods);
+
+  instanceObj.$class = classObj;
+  classObj.$instance = instanceObj;
+
+  if (!configuration) configuration = {};
+
+  if (configuration.attrs) {
+    _.each(configuration.attrs, function(value, key) {
+      classObj.attrs[key] = _.extend({}, attributeObjDefinition, value);
     });
-  */
-  extend: function(configuration, instanceMethods, classMethods) {
-    var instanceObj = _.extend({}, this.$instance, classMethods);
-    var classObj = _.extend({}, this, classMethods);
-
-    instanceObj.$class = classObj;
-    classObj.$instance = instanceObj;
-
-    if (!configuration) configuration = {};
-
-    if (configuration.attrs) {
-      _.each(configuration.attrs, function(value, key) {
-        classObj.attrs[key] = _.extend({}, attributeObjDefinition, value);
-      });
-    }
-
-    return classObj;
   }
+  if (configuration.name) {
+    classObj.name = configuration.name;
+    instanceObj.name = configuration.name;
+  }
+  return classObj;
+}
 };
 
 JsModel.$instance = {
